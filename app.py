525d0076cee21823224c046481255dd882cae183
rajeshchitimireddy <rajesh.chitimireddy@coditas.com>
2021-04-15 15:43:48 +0530

    Added postgreSQL

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,7 +9,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False #changes are tracked by flask we disabled as sqlalchemy tracks this changes
 app.secret_key = 'rajesh'
 api =Api(app)
